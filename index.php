9a4e6601a545c7f59395616ecf599819bbb65c70
Vladimir Shestakov <boolive@yandex.ru>
2014-10-03 19:24:47 +0600

    #new add index and htaccess

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Boolive skeleton
+ * Запуск проекта
+ *
+ * @version 2
+ * @author Vladimir Shestakov <boolive@yandex.ru>
+ * @link http://boolive.ru
+ */
+use boolive\core\Core;
+
+/** @const Времея начала работы системы */
+define('START_TIME', microtime(true));
+/** @cont bool Установлена ли система Boolive? */
+define('IS_INSTALL', true);
+/** @const Версия системы Boolive */
+define('VERSION', '2.0.beta.2014.02.10');
+/** @cont string Директория сайта на сервере. Слеш в конце обязателен! */
+define('DIR', __DIR__.'/');
+/** @const string Директория временных файлов на сервере. Слеш в конце обязателен! */
+define('DIR_TEMP', DIR.'temp/');
+/** @const string Директория конфигураций. Слеш в конце обязателен! */
+define('DIR_CONFIG', DIR.'config/');
+// Адрес сайта, например: boolive.ru. Значение по умолчанию для CLI режима
+define('HTTP_HOST', empty($_SERVER['HTTP_HOST'])?'boolive.ru' : $_SERVER['HTTP_HOST']);
+/* Признак, выводить всю трассировку?*/
+define('GLOBAL_TRACE', true);
+/* Признак, профилировать запросы к модулю даных?*/
+define('PROFILE_DATA', false);
+
+// Get composer class loader
+$loader = include DIR.'vendor/autoload.php';
+// Activate boolive
+Core::activate($loader);
+// start project
+echo Core::start();
