0704c398a9913bb834cbd9554312e34ea814cf4f
Vladimir Shestakov <boolive@yandex.ru>
2014-10-10 19:33:15 +0600

    #enh interfaces

---
+++ interfaces/interfaces.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 
+ * @author Vladimir Shestakov
+ * @version 1.0
+ */
+namespace interfaces;
+
+use boolive\basic\controller\controller;
+
+class interfaces extends controller
+{
+    function work()
+    {
+        return 'MyMy';
+    }
+}
+ 
