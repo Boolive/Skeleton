a3674f7ba84a220167654bafec15767aa6273b33
Vladimir Shestakov <boolive@yandex.ru>
2014-10-28 09:31:23 +0500

    added store config

---
+++ config/stores.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Конфигурация модели данных
+ */
+return array(
+    '' => [
+        'class' => '\boolive\core\data\stores\FilesystemStore',
+        'params'=> [
+
+        ]
+    ],
+);
