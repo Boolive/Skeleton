18cb19b021f34e385147b200ec7dde4439a00ebf
Vladimir Shestakov <boolive@yandex.ru>
2014-10-29 16:34:45 +0500

    test for readdir

--- index.php
+++ index.php
@@ -36,35 +36,130 @@ $loader = include DIR.'vendor/autoload.php';
 Core::activate($loader);
 // Start project (read and call root object)
 //echo Data::read('/interfaces')->start(new Request());
-//trace(Data::find([
-//    'from' => '/interfaces',
-//    'select' => 'children',
-//    'depth' => 14
-//]));
+trace(Data::find([
+    'from' => '/interfaces',
+    'select' => 'children',
+    'depth' => 1
+]));
+//$bench_cnt = 100;
+//
+//$test = 3;
+//
+////$dir = 'C:/Server/Sites/usurt/';
+//
+//// opendir
+//if ($test == 1){
+//    echo '#1<br>';
+//    for ($i=0; $i<$bench_cnt; $i++){
+//        $uri = '';
+//        $depth = 1000;
+//        if ($uri===''){
+//            $dir = DIR;
+//        }else{
+//            $dir = DIR.trim($uri,'/').'/';
+//        }
+//        $objects = [];
+//        // Открыть известный каталог и начать считывать его содержимое
+//        $ignore = array_flip(['.','..','.git']);
+//        if ($dh = opendir($dir)) {
+//            $stack = [['dir' => $dir, 'name' => '', 'dh' => $dh, 'depth' => $depth, 'parent' => $uri]];
+//            do {
+//                $curr = end($stack);
+//                while (($name = readdir($curr['dh'])) !== false) {
+//                    if (!isset($ignore[$name])) {
+//                        $uri = ($curr['name']==='')? $curr['parent'] : $curr['parent'].'/'.$curr['name'];
+//                        if ($name == $curr['name'] . '.info') {
+//                            $objects[] = $uri;
+//                        } else
+//                        if ($curr['depth'] && is_dir($path = $curr['dir'] . $name)) {
+//                            if ($dh = opendir($path . '/')) {
+//                                $stack[] = ['dir' => $path . '/', 'name' => $name, 'dh' => $dh, 'depth' => $curr['depth'] - 1, 'parent' => $uri];
+//                                $curr = end($stack);
+//                            }
+//                        }
+//                    }
+//                }
+//                closedir($curr['dh']);
+//                array_pop($stack);
+//            } while ($stack);
+//        }
+//    }
+//}
+//// glob
+//if ($test == 2){
+//    echo '#2<br>';
+//    $glob = function($dir) use (&$glob)
+//    {
+//        $dirs = glob($dir.'*', GLOB_ONLYDIR|GLOB_NOSORT);
+//        $objects = [];
+//        foreach ($dirs as $dir){
+//            $name = basename($dir);
+//            if (!isset($ignore[$name])) {
+//                if (is_file($dir.'/'.$name.'.info')){
+//                    $objects[] = $dir;
+//                }
+//                $objects = array_merge($objects, $glob($dir.'/'));
+//            }
+//        }
+//        return $objects;
+//    };
+//    for ($i=0; $i<$bench_cnt; $i++){
+//        $uri = '';
+//        $depth = 1000;
+//        if ($uri===''){
+//            $dir = DIR;
+//        }else{
+//            $dir = DIR.trim($uri,'/').'/';
+//        }
+//        $objects = $glob($dir);
+//    }
+//}
+////
+//if ($test == 3){
+//    echo '#3<br>';
+//    //for ($i=0; $i<$bench_cnt; $i++){
+//        $uri = '';
+//        $depth = 1000;
+//        if ($uri===''){
+//            $dir = DIR;
+//        }else{
+//            $dir = DIR.trim($uri,'/').'/';
+//        }
+//        $objects = [];
+//        // Открыть известный каталог и начать считывать его содержимое
+//        $ignore = array_flip(['.','..','.git']);
+//        if ($dh = new DirectoryIterator($dir)) {
+//            $stack = [['name' => '', 'dh' => $dh, 'depth' => $depth, 'parent' => $uri]];
+//            do {
+//                $curr = end($stack);
+//                while($curr['dh']->valid()){
+//                    /** @var DirectoryIterator $file */
+//                    $file = $curr['dh']->current();
+//                    $curr['dh']->next();
+//                    if (($name = $file->getFilename())!==''){
+//                        if (!isset($ignore[$name])) {
+//                            $uri = ($curr['name']==='')? $curr['parent'] : $curr['parent'].'/'.$curr['name'];
+//                            if ($name == $curr['name'] . '.info') {
+//                                $objects[] = $uri;
+//                            } else
+//                            if ($curr['depth'] && $file->isDir()) {
+//                                if ($dh = new DirectoryIterator($file->getPathname())) {
+//                                    $stack[] = ['name' => $name, 'dh' => $dh, 'depth' => $curr['depth'] - 1, 'parent' => $uri];
+//                                    $curr = end($stack);
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//                array_pop($stack);
+//            } while ($stack);
+//        }
+//    //}
+//}
+//
+//foreach($objects as $file_or_dir) {
+//    echo $file_or_dir . '<br>';
+//}
+//
+//trace(\boolive\core\develop\Benchmark::stop('all', true));
 
-$dir = DIR."";
-// Открыть известный каталог и начать считывать его содержимое
-$ignore = array_flip(['.','..','.git']);
-$depth = 10;
-
-if ($dh = opendir($dir)) {
-    $stack = [['dir' => $dir, 'name' => basename($dir), 'dh' => $dh, 'depth' => $depth]];
-    do {
-        $curr = end($stack);
-        while (($file = readdir($curr['dh'])) !== false) {
-            if (!isset($ignore[$file])) {
-                if ($file == $curr['name'] . '.info') {
-                    echo 'Object: ' . $curr['dir'] . '<br>';
-                }else
-                if ($curr['depth'] && is_dir($path = $curr['dir'] . $file)) {
-                    if ($dh = opendir($path . '/')) {
-                        $stack[] = ['dir' => $path . '/', 'name' => $file, 'dh' => $dh, 'depth' => $curr['depth'] - 1];
-                        $curr = end($stack);
-                    }
-                }
-            }
-        }
-        closedir($curr['dh']);
-        array_pop($stack);
-    } while ($stack);
-}
