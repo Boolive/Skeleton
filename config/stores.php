78240ca54816a4f6ea4625b08e4222631a6e5af7
VladimirShestakov <boolive@yandex.ru>
2015-11-05 04:49:30 +0500

    test store with MySQLStore

--- config/stores.php
+++ config/stores.php
@@ -9,4 +9,30 @@ return array(
 
         ]
     ],
+    '/test' => [
+        'class' => '\boolive\core\data\stores\MySQLStore',
+        'params' => array(
+            // Имя источника данных
+            'dsn' => array(
+                // Тип СУБД
+                'driver' => 'mysql',
+                // Имя базы данных
+                'dbname' => 'skeleton',
+                // Адрес сервера
+                'host' => '127.0.0.1',
+                // Порт
+                'port' => '3306'
+            ),
+            // Имя пользователя для подключения к базе данных
+            'user' => 'root',
+            // Пароль
+            'password' => '',
+            // Опции подключения
+            'options' => array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES "utf8" COLLATE "utf8_bin"'
+            ),
+            // Префикс к таблицам
+            'prefix' => '',
+        )
+    ]
 );
